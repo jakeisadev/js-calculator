4a535f8e434dff90c878cf7789fb197acbb2979e
jakeisadev <starowicz@protonmail.com>
2022-03-22 14:00:03 -0400

    updated js and html

--- script.js
+++ script.js
@@ -1,18 +1,51 @@
-(function() {
-    "use strict";
-    let el = function(element) {
-        if(element.charAt(0) === "#") {
-            return document.querySelector(element);
-        }
-        return document.querySelectorAll(element);
-    };
-});
-
-let viewer = el("#viewer"),
-equals = el("equals"),
-nums = el(".main-num"),
-ops = el("operator"),
-theNum = "",
-oldNum = "",
-resultNum,
-operator;
+const calculator = {
+    displayValue: '0',
+    firstOperand: null,
+    waitingForSecondOperand: false,
+    operator: null,
+  };
+  
+  function inputDigit(digit) {
+    const { displayValue } = calculator;
+    calculator.displayValue = displayValue === '0' ? digit : displayValue + digit;
+  }
+  
+  function inputDecimal(dot) {
+    if (!calculator.displayValue.includes(dot)) {
+      calculator.displayValue += dot;
+    }
+  }
+  
+  function updateDisplay() {
+    const display = document.querySelector('.viewer');
+    display.value = calculator.displayValue;
+  }
+  
+  updateDisplay();
+  
+  const keys = document.querySelector('.calculator');
+  keys.addEventListener('click', (event) => {
+    const { target } = event;
+    if (!target.matches('button')) {
+      return;
+    }
+  
+    if (target.classList.contains('operator')) {
+      console.log('operator', target.value);
+      return;
+    }
+  
+    if (target.classList.contains('decimal')) {
+      inputDecimal(target.value);
+          updateDisplay();
+      return;
+    }
+  
+    if (target.classList.contains('all-clear')) {
+      console.log('clear', target.value);
+      return;
+    }
+  
+    inputDigit(target.value);
+    updateDisplay();
+  });
